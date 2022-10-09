15555e61ad8a379065b209713ffebf2c10795776
dopecello <williamlocke.cello@gmail.com>
2022-10-08 21:20:53 -0400

    rough draft of User model... able to test

--- models/User.js
+++ models/User.js
@@ -1,10 +1,49 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema(
-    {
-        username: {},
-        email: {},
-        thoughts: {},
-        friends: {},
-    }
-)
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+        },
+        message: "Please enter a valid email!",
+      },
+      required: [true, "Email required."],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.reduce((total) => total + 1, 0);
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
