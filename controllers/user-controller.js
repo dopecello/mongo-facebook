42daf36f6fb5b67534478342406bc6e8955ef008
dopecello <williamlocke.cello@gmail.com>
2022-10-11 13:34:18 -0400

    user-setup complete. all routes tested and verified

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const { User } = require("../models");
 
 const userController = {
+
   //get all users
   getAllUsers(req, res) {
     User.find({})
@@ -11,9 +12,43 @@ const userController = {
         res.sendStatus(400);
       });
   },
+
+  //get a user by an id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
   //create a user
   createUser({ body }, res) {
     User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+
+  //update user 
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  //delete user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.json(err));
   },
