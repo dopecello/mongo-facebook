ba0c56cb87442cde225bfde508692ded6ad4c6db
dopecello <williamlocke.cello@gmail.com>
2022-11-17 09:04:35 -0500

    fixed user update by id bug

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,7 +10,7 @@ const {
 } = require("../../controllers/user-controller");
 
 router.route("/").get(getAllUsers).post(createUser);
-router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
-router.route("/:userId/friends/:friendId").put(addFriend).delete(removeFriend);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
